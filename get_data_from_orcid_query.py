d3ebc2d543ad7f6ebae3436d6a0755348a6de2ab
Kajetan <87271512+kajoo8@users.noreply.github.com>
2024-11-17 00:08:36 +0100

    Delete get_data_from_orcid_query.py

--- get_data_from_orcid_query.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def determine_orcid_browse_type(link: str) -> str:
-    """Insert type info based on the string link"""
-    pattern = r"https?://orcid\.org/\d{4}-\d{4}-\d{4}-\d{4}"
-    if re.match(pattern, link):
-        return "profile"
-    else:
-        return "unknown"
